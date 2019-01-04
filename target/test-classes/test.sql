e88ed27899a339d4d8a09f65dc9f17bcf086d12e
DanielOrDany <nikulshyn.daniel@gmail.com>
2019-01-04 23:29:56 +0200

    Beta version - Eventor 1.0

---
+++ target/test-classes/test.sql
@@ -0,0 +1,6 @@
+INSERT INTO events (title,whenStart,whereStart,description,speaker,speakerAbout,organizer,organizerAbout,category, link)
+VALUES ('Workshop NULP','25.05.2018','S','V','B','A','R','T','workshops','link');
+#select * from events;
+#select * from events where title = 'SoftServe';
+#update events set title = 'NULP' where id = 1;
+#delete from events where id = 1;
